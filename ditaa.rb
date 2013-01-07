6cea076009ec3f3e148ca2ecf3b8160105b77148
Andrew Paradise <adparadise@gmail.com>
2013-01-07 14:27:24 -0500

    Update ditaa.rb
    Fix to text cleanup to match stray characters I was seeing, perhaps caused by Ruby 1.9.
    Tweak IMG tag rendering to allow images to be served if they exist on disk.

--- ditaa.rb
+++ ditaa.rb
@@ -7,28 +7,32 @@ module Jekyll
       super
 
       @ditaa_exists = system('which ditaa > /dev/null 2>&1')
-      @result = True
+
+      # There is always a blank line at the beginning, so we remove to get rid
+      # of that undesired top padding in the ditaa output
+      ditaa = @nodelist.to_s
+      ditaa.gsub!('\n', "\n")
+      ditaa.gsub!(/^\[\"\n/, "")
+      ditaa.gsub!(/\"\]$/, "")
+      ditaa.gsub!(/\n+$/, "")
+
+      hash = Digest::MD5.hexdigest(@nodelist.to_s)
+      ditaa_home = 'images/ditaa/'
+      FileUtils.mkdir_p(ditaa_home)
+      @png_name = ditaa_home + 'ditaa-' + hash + '.png'
 
       if @ditaa_exists
-        # No cheesy separations and shadows
-        options = ' -E -S -o'
-
-        # There is always a blank line at the beginning, so we remove to get rid
-        # of that undesired top padding in the ditaa output
-        ditaa = @nodelist.to_s.gsub(/^$\n/, '')
-
-        hash = Digest::MD5.hexdigest(@nodelist.to_s)
-        @png_name = 'images/ditaa/ditaa-' + hash + '.png'
-
         if not File.exists?(@png_name)
+          options = ' -E -s2 -o'
           File.open('/tmp/ditaa-foo.txt', 'w') {|f| f.write(ditaa)}
-          @result = system('ditaa /tmp/ditaa-foo.txt ' + @png_name + options)
+          @png_exists = system('ditaa /tmp/ditaa-foo.txt ' + @png_name + options)
         end
       end
+      @png_exists = File.exists?(@png_name)
     end
 
     def render(context)
-      if @ditaa_exists and @result
+      if @png_exists
         '<figure><img src="/' + @png_name + '" /></figure>'
       else
         '<code><pre>' + super + '</pre></code>'
