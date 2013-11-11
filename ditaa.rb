c1be599ffca2ee8de8614d57013ea52a1363f798
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-11-11 10:46:54 +0100

    Use Jekyll:StaticFile for copying

--- ditaa.rb
+++ ditaa.rb
@@ -3,40 +3,52 @@ require 'digest/md5'
 
 module Jekyll
   class DitaaBlock < Liquid::Block
+
+    @@globals = {
+      "output_dir" => "images/ditaa/"
+    }
+
     def initialize(tag_name, options, tokens)
       super
 
-      ditaa_exists = system('which ditaa > /dev/null 2>&1')
-
-      # There is always a blank line at the beginning, so we remove to get rid
-      # of that undesired top padding in the ditaa output
-      ditaa = @nodelist.to_s
-      ditaa.gsub!('\n', "\n")
-      ditaa.gsub!(/^$\n/, "")
-      ditaa.gsub!(/^\[\"\n/, "")
-      ditaa.gsub!(/\"\]$/, "")
-      ditaa.gsub!(/\\\\/, "\\")
-
-      hash = Digest::MD5.hexdigest(@nodelist.to_s + options)
-      ditaa_home = 'images/ditaa/'
-      FileUtils.mkdir_p(ditaa_home)
-      @png_name = ditaa_home + 'ditaa-' + hash + '.png'
-
-      if ditaa_exists
-        if not File.exists?(@png_name)
-          args = ' ' + options + ' -o'
-          File.open('/tmp/ditaa-foo.txt', 'w') {|f| f.write(ditaa)}
-          @png_exists = system('ditaa /tmp/ditaa-foo.txt ' + @png_name + args)
-        end
-      end
-      @png_exists = File.exists?(@png_name)
+      @ditaa_exists = system('which ditaa > /dev/null 2>&1')
+      @ditaa_options = options
     end
 
     def render(context)
-      if @png_exists
-        '<figure><a href="/' + @png_name + '" title="' + @png_name + '" ><img src="/' + @png_name + '" title="' + @png_name + '" max-width="99%" /></a></figure>'
+      source = super
+
+      # There is always a blank line at the beginning, so we remove to get rid
+      # of that undesired top padding in the ditaa output
+      source.gsub!('\n', "\n")
+      source.gsub!(/^$\n/, "")
+      source.gsub!(/^\[\"\n/, "")
+      source.gsub!(/\"\]$/, "")
+      source.gsub!(/\\\\/, "\\")
+
+      hash = Digest::MD5.hexdigest(source + @ditaa_options)
+
+      # ditaa_home = 'images/ditaa/'
+      FileUtils.mkdir_p(@@globals["output_dir"]) unless File.exists?(@@globals["output_dir"])
+
+      @png_name = 'ditaa-' + hash + '.png' #ditaa_home + 'ditaa-' + hash + '.png'
+      @png_path = @@globals["output_dir"] + @png_name
+
+      if @ditaa_exists
+        if not File.exists?(@png_path)
+          args = ' ' + @ditaa_options + ' -o'
+          File.open('/tmp/ditaa-foo.txt', 'w') {|f| f.write(source)}
+          @png_exists = system('ditaa /tmp/ditaa-foo.txt ' + @png_path + args)
+        end
+      end
+
+      if File.exists?(@png_path)
+        site = context.registers[:site]
+        site.static_files << Jekyll::StaticFile.new(site, site.source, @@globals["output_dir"], @png_name)
+
+        '<figure><a href="/' + @png_path + '" title="' + @png_name + '" ><img src="/' + @png_path + '" title="' + @png_name + '" max-width="99%" /></a></figure>'
       else
-        '<code><pre>' + super + '</pre></code>'
+        '<code><pre>' + source + '</pre></code>'
       end
     end
   end
